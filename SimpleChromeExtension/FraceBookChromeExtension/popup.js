c97f3fb34d19b861b3d05c40ee33bbb327c1bd22
Frace Marteja <programmertv2021@gmail.com>
2021-09-05 19:28:20 +0800

    Simple Calculator

---
+++ SimpleChromeExtension/FraceBookChromeExtension/popup.js
@@ -0,0 +1,21 @@
+﻿$(function () {
+    $('#btnCalculate').click(function () {        
+        let num1 = parseInt($('#input1').val());
+        let num2 = parseInt($('#input2').val());
+        let op = $('#operator').val();
+        let resultPlaceholder = $('#spanResult');
+        switch (op) {
+            case '+':
+                resultPlaceholder.text(num1 + num2);
+                break;
+            case '-':
+                resultPlaceholder.text(num1 - num2);
+                break;
+            case '*': resultPlaceholder.text(num1 * num2);
+                break;
+            case '/':
+                resultPlaceholder.text(num1 / num2);
+                break;
+        }
+    });
+});
